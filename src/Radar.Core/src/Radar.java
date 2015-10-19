90eb3e18bc6f7452f3986240858f44254f21cd7c
Szendaaj <szendaaj@gmail.com>
2015-10-19 21:01:48 +0200

    doksi módosítva

--- src/Radar.Core/src/Radar.java
+++ src/Radar.Core/src/Radar.java
@@ -44,4 +44,68 @@ public class Radar
     {
 
     }
+
+
+    public Pair GetCarPosition(){
+        Pair coordinates;
+        coordinates.setFirst(10);
+        coordinates.setSecond(10);
+        return coordinates;
+    }
+
+    public Pair
+}
+
+public class Pair<X, Y> {
+    private X first;
+    private Y second;
+
+    public Pair(X first, Y second) {
+        super();
+        this.first = first;
+        this.second = second;
+    }
+
+    public int hashCode() {
+        int hashFirst = first != null ? first.hashCode() : 0;
+        int hashSecond = second != null ? second.hashCode() : 0;
+
+        return (hashFirst + hashSecond) * hashSecond + hashFirst;
+    }
+
+    public boolean equals(Object other) {
+        if (other instanceof Pair) {
+            Pair otherPair = (Pair) other;
+            return
+                    ((  this.first == otherPair.first ||
+                            ( this.first != null && otherPair.first != null &&
+                                    this.first.equals(otherPair.first))) &&
+                            (	this.second == otherPair.second ||
+                                    ( this.second != null && otherPair.second != null &&
+                                            this.second.equals(otherPair.second))) );
+        }
+
+        return false;
+    }
+
+    public String toString()
+    {
+        return "(" + first + ", " + second + ")";
+    }
+
+    public X getFirst() {
+        return first;
+    }
+
+    public void setFirst(X first) {
+        this.first = first;
+    }
+
+    public Y getSecond() {
+        return second;
+    }
+
+    public void setSecond(Y second) {
+        this.second = second;
+    }
 }
