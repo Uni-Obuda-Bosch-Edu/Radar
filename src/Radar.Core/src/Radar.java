f50881f50d005431fe9e86ae4ba00b0d922e2c5c
Leimeter Csaba <reptile888888@gmail.com>
2015-10-20 20:38:05 +0200

    radar relative speed, interfaces

--- src/Radar.Core/src/Radar.java
+++ src/Radar.Core/src/Radar.java
@@ -17,6 +17,8 @@ public class Radar {
     private final IRadarBase _radarBase;
     private final Timer _messagePump;
 
+    private double originalDistance = Double.NaN;
+
     public Radar(Container container) {
         _container = container;
         _radarBase = new RadarBase(_container, _container);
@@ -43,38 +45,46 @@ public class Radar {
     }
 
     private void Signal() {
-
+        Point radarPosition = SetRadarPosition(GetCarPosition());
+        List<Point> setTriangle = SetTriangle();
+        Point nearestObjectPosition = GetNearestObject(GetRelevantObjects());
+        _container.setObjectsbyTriangle(setTriangle);
+        _container.setRadarDetectedObject(nearestObjectPosition, RelativeSpeedInKPH(radarPosition, nearestObjectPosition));
     }
 
+    //dummy
+    private List<Point> GetRelevantObjects() {
+        return SetTriangle();
+    }
 
-    public Point GetCarPosition() {
+    private Point GetCarPosition() {
         //dummy adat, majd lekérdezzük a bus-ról a pontos értéket
         Point coordinates=new Point(10,10);
         return coordinates;
     }
 
-    public Point SetRadarPosition(Point CarPosition){
-      
+    protected Point SetRadarPosition(Point CarPosition){
+
         return new Point(CarPosition.x, CarPosition.y+2);
-   }
+    }
 
     //getobjectsbytriangle
-    public List<Point> SetTriangle(){
-        Point radarposition=SetRadarPosition(GetCarPosition());
+    protected List<Point> SetTriangle(){
+        Point radarPosition = SetRadarPosition(GetCarPosition());
         List<Point>TriangleCoordinates=new ArrayList<>();
         //A - radar pozi
-        TriangleCoordinates.add(radarposition); 
+        TriangleCoordinates.add(radarPosition);
         //B - bal felső sarok
-        TriangleCoordinates.add(new Point(radarposition.x-10, radarposition.y+10)); 
+        TriangleCoordinates.add(new Point(radarPosition.x-10, radarPosition.y+10));
         //C - jobb felső sarok
-        TriangleCoordinates.add(new Point(radarposition.x+10, radarposition.y+10));
+        TriangleCoordinates.add(new Point(radarPosition.x+10, radarPosition.y+10));
               
         return TriangleCoordinates;
     }
 
     //eldöntöm melyik van a legközelebb
     //most csak koordinátákkal dolgozom, később átírni a világtól kapott objektumoknak megfelelően
-    public Point GetNearestObject(List<Point> objects){
+    protected Point GetNearestObject(List<Point> objects){
         Point CarPosition= GetCarPosition();
         Point first=objects.get(0);
         double min=Math.sqrt(Math.pow(doubleValue(first.y-CarPosition.y),2)+Math.pow(doubleValue(first.x-CarPosition.x),2));
@@ -90,6 +100,17 @@ public class Radar {
         
         return objects.get(idx);
     }
-    
+
+    protected double RelativeSpeedInKPH(Point radarPos, Point nearestPos)
+    {
+        if (!Double.isNaN(originalDistance))
+        {
+            return originalDistance - Math.sqrt(Math.pow(doubleValue(nearestPos.y - radarPos.y), 2) + Math.pow(doubleValue(nearestPos.x - radarPos.x), 2)) * 3.6;
+        }
+
+        originalDistance = Math.sqrt(Math.pow(doubleValue(nearestPos.y - radarPos.y), 2) + Math.pow(doubleValue(nearestPos.x - radarPos.x), 2));
+
+        return Double.NaN;
+    }
 }
 
